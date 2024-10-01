f16d4627ee70e7624782f596f73fd6704af3cb12
Allan Gallo <allan.gallo.guerra@gmail.com>
2024-10-01 17:12:00 -0600

    add the create method for users and rename test file for the ping endpoint

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -26,3 +26,23 @@ func (uc *UserController) GetUsers(c *gin.Context) {
 
 	c.JSON(http.StatusOK, gin.H{"success": true, "data": users})
 }
+
+func (uc *UserController) CreateUser(context *gin.Context) {
+	var user models.User
+
+	error := context.ShouldBindJSON(&user)
+
+	if error != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "User validation falied!", "error": error.Error()})
+		return
+	}
+
+	result := uc.DB.Create(&user)
+
+	if result.Error != nil {
+		context.JSON(http.StatusBadRequest, gin.H{"success": false, "message": "User validation falied!", "error": result.Error.Error()})
+		return
+	}
+
+	context.JSON(http.StatusOK, gin.H{"success": true})
+}
